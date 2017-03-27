927dd2e9297967d6b787964853d2894499930501
bindupothana <himabindu.pothana@navaratan.com>
2017-03-27 11:16:39 +0530

    Armstrong Number

--- script/app.js
+++ script/app.js
@@ -1,15 +1,15 @@
 var app=angular.module('armApp',[]);
 app.controller('armCtrl',function($scope){
 	$scope.armstrong=function(num){
-	var rem,z,c=0;
-z=num;
-while(z>0)
+	var rem,temp,a=0;
+temp=num;
+while(temp>0)
 {
-rem=z%10;
-c=c+(rem*rem*rem);
-z=parseInt(z/10);
+rem=temp%10;
+a=a+(rem*rem*rem);
+temp=parseInt(temp/10);
 }
-if(num==c)
+if(num==a)
 $scope.message="given no is amstrong number";
 else
 $scope.message="given no is not an amstrong number";	 
